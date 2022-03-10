6f284436b5a29af658643ce2c4b75308aba96e6c
Wangert <joten_wang@qq.com>
2022-03-10 12:35:51 +0800

    resolving conflicts with develop branch

--- main/src/main.rs
+++ main/src/main.rs
@@ -7,6 +7,10 @@ use tokio::{
 };
 use utils::parse::into_ip4_tcp_multiaddr;
 
+use core::blockchain;
+use std::thread;
+use std::time::Duration;
+
 #[tokio::main]
 async fn main() {
     let args: Args = Args::from_args();
@@ -36,4 +40,30 @@ async fn main() {
     });
 
     local_peer.run(&mut rx).await;
+
+    // let mut local_peer = Peer::new("/ip4/10.162.133.179/tcp/51002".parse().unwrap(), 31001);
+    // let keys = local_peer.get_keys().clone();
+    // let mut mdns_swarm = MdnsSwarm::new(&keys);
+
+    // let mut gossipsub_swarm = GossipsubSwarm::new(&keys);
+
+    // let (f1, f2) = run(&mut local_peer, &mut mdns_swarm, &mut gossipsub_swarm).await;
+
+    // match (f1, f2) {
+    //     (Ok(_), Ok(_)) => println!("Peer runing successful!"),
+    //     _ => panic!("Peer running error!"),
+    // }
+    //run(&mut local_peer);
+
+    //println!("Hello, world!");
+    let mut bc = blockchain::BlockChain::new_blockchain();
+
+    println!("第一轮共识进行中。。。。。。。。。");
+    thread::sleep(Duration::from_secs(5));
+    bc.add_block(String::from("aa"));
+
+    println!("");
+    println!("第二轮共识进行中。。。。。。。。。");
+    thread::sleep(Duration::from_secs(5));
+    bc.add_block("bb".to_string());
 }
